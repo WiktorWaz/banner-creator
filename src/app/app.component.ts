793df17d0634efb867b3d3971bd8926d13b2ebb5
Wiktor <wiktor.wazniewski@gmail.com>
2021-03-17 15:34:29 +0100

    Final version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import domtoimage from 'dom-to-image';
+import { saveAs } from 'file-saver';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'banner-creator';
+
+  date = new Date();
+
+  bannerHeightVal = 600;
+  bannerWidthVal = 600;
+  bannerFontSizeVal = 55;
+  colorOne = 'red';
+  colorTwo = 'purple';
+  colorOnePer = 20;
+  colorTwoPer = 80; 
+  fontColor = 'white';
+  font = 'Courier';
+
+  colors = [
+    'white',
+    'yellow',
+    'red',
+    'purple',
+    'green',
+    'gray',
+    'brown',
+    'black',
+  ];
+
+  bannerStyle() {
+    return {   
+    width: this.bannerWidthVal + 'px',
+    height: this.bannerHeightVal + 'px',
+    background: 'linear-gradient(' + this.colorOne + ' ' + this.colorOnePer + '%, ' + this.colorTwo + ' ' + this.colorTwoPer + '%)',
+    }
+  };
+
+  fontStyle() {
+    return {
+    fontSize: this.bannerFontSizeVal + 'px',
+    width: this.bannerWidthVal + 'px',
+    height: this.bannerHeightVal + 'px',
+    color: this.fontColor,
+    fontFamily: this.font,
+    }
+  };
+
+  downloadImg(bannerView: HTMLDivElement ) {
+    domtoimage.toBlob(bannerView).then((blob) => saveAs(blob, 'banner.jpg'));
+    };
+
 }
